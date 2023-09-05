80f55cfd2c46c15e9f4160dfb1e9837340f433e0
Khanh Lam <quockhanhdev@gmail.com>
2023-09-05 23:37:06 +0700

    update test

--- libs/infrastructures/src/config/config.service.spec.ts
+++ libs/infrastructures/src/config/config.service.spec.ts
@@ -11,10 +11,10 @@ describe('ConfigService', () => {
       NODE_ENV: 'development',
       PORT: '9200',
       MONGO_DB_URI: 'mongodb://localhost:27017/mydb',
-      AUTH_JWT_ACCESS_TOKEN_SECRET: 'access-token-secret',
-      AUTH_JWT_ACCESS_TOKEN_EXPIRED: '15m',
-      AUTH_JWT_REFRESH_TOKEN_SECRET: 'refresh-token-secret',
-      AUTH_JWT_REFRESH_TOKEN_EXPIRED: '30d',
+      JWT_ACCESS_TOKEN_SECRET: 'access-token-secret',
+      JWT_ACCESS_TOKEN_EXPIRATION_TIME: '900',
+      JWT_REFRESH_TOKEN_SECRET: 'refresh-token-secret',
+      JWT_REFRESH_TOKEN_EXPIRATION_TIME: '2592000',
     };
 
     const module = await Test.createTestingModule({
@@ -39,9 +39,9 @@ describe('ConfigService', () => {
   });
 
   it('should have correct values for jwt properties', () => {
-    expect(configService.jwt.atSecret).toBe('access-token-secret');
-    expect(configService.jwt.atExpired).toBe('15m');
-    expect(configService.jwt.rtSecret).toBe('refresh-token-secret');
-    expect(configService.jwt.rtExpired).toBe('30d');
+    expect(configService.jwt.accessToken.secret).toBe('access-token-secret');
+    expect(configService.jwt.accessToken.expirationTime).toBe(900);
+    expect(configService.jwt.refreshToken.secret).toBe('refresh-token-secret');
+    expect(configService.jwt.refreshToken.expirationTime).toBe(2592000);
   });
 });
